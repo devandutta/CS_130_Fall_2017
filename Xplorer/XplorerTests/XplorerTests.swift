460eff6ad6e2f69f63f9fda1c923077efc195e37
Avirudh Theraja <avirudhtheraja@gmail.com>
2017-11-16 16:42:24 -0800

    Add unit tests, change Podfile to use GoogleApis in unit tests
    Some other minor fixes such as assigning an identifier to the MapViewController

--- Xplorer/XplorerTests/XplorerTests.swift
+++ Xplorer/XplorerTests/XplorerTests.swift
@@ -8,20 +8,29 @@
 
 import UIKit
 import XCTest
+import GoogleMaps
+import GooglePlaces
 @testable import Xplorer
 
 class XplorerTests: XCTestCase {
     
     // Properties
-    var systemUnderTest:TimeAndLocationViewController!
+    var timeAndLocationController:TimeAndLocationViewController!
+    var autocompleteController:GMSAutocompleteViewController!
+    var mapController:MapViewController!
+    // Dummy place, a hotel in Saigon with an attribution.
+    var placeID = "ChIJV4k8_9UodTERU5KXbkYpSYs"
     
     override func setUp() {
         super.setUp()
         // Put setup code here
         let storyboard = UIStoryboard(name:"Main", bundle:nil)
-        systemUnderTest = storyboard.instantiateViewController(withIdentifier: "timeAndLocation") as! TimeAndLocationViewController
+        timeAndLocationController = storyboard.instantiateViewController(withIdentifier: "timeAndLocation") as! TimeAndLocationViewController
+        autocompleteController = GMSAutocompleteViewController()
+        self.mapController = storyboard.instantiateViewController(withIdentifier: "mapView") as! MapViewController
         // run view did load
-        _ = systemUnderTest.view
+        _ = timeAndLocationController.view
+        _ = mapController.view
     }
     
     override func tearDown() {
@@ -29,17 +38,94 @@ class XplorerTests: XCTestCase {
         super.tearDown()
     }
     
-    func testTimeAndLocationViewIsNotNilAfterViewDidLoad(){
-        XCTAssertNotNil(systemUnderTest.view)
+    func testTimeAndLocationControllerNotNull() {
+        XCTAssertNotNil(timeAndLocationController)
     }
     
+    func testAutoCompleteViewControllerNotNull(){
+        XCTAssertNotNil(autocompleteController)
+    }
+    
+    func testMapControllertNotNull(){
+        XCTAssertNotNil(mapController)
+    }
+    
+    func testTimeAndLocationViewIsNotNilAfterViewDidLoad(){
+        XCTAssertNotNil(timeAndLocationController.view)
+    }
+    
+    func testMapViewIsNotNilAfterViewDidLoad(){
+        XCTAssertNotNil(mapController.view)
+    }
     
     func testStartLocationNotNilAfterViewDidLoad(){
-        XCTAssertNotNil(systemUnderTest.startLocation)
+        XCTAssertNotNil(timeAndLocationController.startLocation)
     }
     
-    // TODO: test start location is set when AutoCompleteViewController is called
-    // TODO: test end location is set
+    func testStartPlaceGetsSelected(){
+        mapController.placesClient.lookUpPlaceID(placeID, callback: { (place, error) -> Void in
+            if let error = error {
+                print("lookup place id query error: \(error.localizedDescription)")
+                XCTFail()
+            }
+            
+            guard let place = place else {
+                print("No place details for \(self.placeID)")
+                XCTFail()
+                return
+            }
+            self.timeAndLocationController.lastUITextFieldSelected = self.timeAndLocationController.startLocation
+            self.timeAndLocationController.endPlace = nil
+            self.timeAndLocationController.viewController(self.autocompleteController, didAutocompleteWith: place)
+            XCTAssertNotNil(self.timeAndLocationController.startPlace)
+            XCTAssertFalse(self.timeAndLocationController.doneButton.isEnabled)
+            XCTAssertNil(self.timeAndLocationController.lastUITextFieldSelected)
+            XCTAssertNil(self.timeAndLocationController.endPlace)
+        })
+    }
     
+    func testEndPlaceGetsSelected(){
+        mapController.placesClient.lookUpPlaceID(placeID, callback: { (place, error) -> Void in
+            if let error = error {
+                print("lookup place id query error: \(error.localizedDescription)")
+                XCTFail()
+            }
+            
+            guard let place = place else {
+                print("No place details for \(self.placeID)")
+                XCTFail()
+                return
+            }
+            self.timeAndLocationController.lastUITextFieldSelected = self.timeAndLocationController.endLocation
+            self.timeAndLocationController.startPlace = nil
+            self.timeAndLocationController.viewController(self.autocompleteController, didAutocompleteWith: place)
+            XCTAssertNotNil(self.timeAndLocationController.endPlace)
+            XCTAssertFalse(self.timeAndLocationController.doneButton.isEnabled)
+            XCTAssertNil(self.timeAndLocationController.startPlace)
+            XCTAssertNil(self.timeAndLocationController.lastUITextFieldSelected)
+        })
+    }
     
+    func testDoneButtonEnablesAfterBothPlacesAreSelected(){
+        mapController.placesClient.lookUpPlaceID(placeID, callback: { (place, error) -> Void in
+            if let error = error {
+                print("lookup place id query error: \(error.localizedDescription)")
+                XCTFail()
+            }
+            
+            guard let place = place else {
+                print("No place details for \(self.placeID)")
+                XCTFail()
+                return
+            }
+            self.timeAndLocationController.startPlace = place
+            self.timeAndLocationController.endPlace = place
+            self.timeAndLocationController.viewController(self.autocompleteController, didAutocompleteWith: place)
+            XCTAssertTrue(self.timeAndLocationController.doneButton.isEnabled)
+        })
+    }
+    
+    func testTextFieldShouldReturn() {
+        XCTAssertTrue(timeAndLocationController.textFieldShouldReturn(UITextField()))
+    }
 }
